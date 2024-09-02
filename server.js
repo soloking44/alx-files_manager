c5441ce0afdbf0ae73a1b2abc5d5c5db26027f12
soloking44 <solop8483@gmail.com>
2024-09-02 17:04:27 +0000

    server.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ import express from 'express';
 import router from './routes';
 
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json({ limit: '100mb' }));
 app.use(router);
+app.listen(process.env.PORT || 5000);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+export default app;

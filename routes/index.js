c47113d6c0eb4e20b53dd2ef7853a446e8899a73
soloking44 <solop8483@gmail.com>
2024-09-02 17:08:38 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,22 @@
-import {
-  Router,
-} from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
-// check status and stats of db
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// connect and disconnect user
+router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-
-// upload files
+router.get('/users/me', AuthController.getMe);
 router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-
-// publish and unpublish
+router.get('/files/:id', FilesController.getShow);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-// user
-router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
-// file content
 router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
